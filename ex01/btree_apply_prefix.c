a39ab90a7692b10f00c937e4bebfcb3c060d6626
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 07:29:27 +0800

    done with ex01

--- ex01/btree_apply_prefix.c
+++ ex01/btree_apply_prefix.c
@@ -6,7 +6,35 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 07:22:46 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 07:22:47 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 07:29:11 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_btree.h"
+#include <stdlib.h>
+
+t_btree *btree_create_node(void *item)
+{
+    t_btree *new_node;
+
+    new_node = (t_btree *)malloc(sizeof(t_btree));
+    if (new_node == NULL)
+        return (NULL);
+    new_node->item = item;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return (new_node);
+}
+
+void btree_apply_prefix(t_btree *root, void (*applyf)(void *))
+{
+    if (root)
+    {
+        (*applyf)(root->item);
+
+        btree_apply_prefix(root->left, applyf);
+
+        btree_apply_prefix(root->right, applyf);
+
+    }
+}
