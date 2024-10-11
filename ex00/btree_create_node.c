d5a570db22730d2bea35ee264b17781e289e4e58
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 07:26:53 +0800

    done with ex00

--- ex00/btree_create_node.c
+++ ex00/btree_create_node.c
@@ -6,7 +6,22 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 07:22:00 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 07:22:01 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 07:26:13 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include "ft_btree.h"
+
+t_btree *btree_create_node(void *item)
+{
+    t_btree *new_node;
+
+    new_node = (t_btree *)malloc(sizeof(t_btree));
+    if (new_node == NULL)
+        return (NULL);
+    new_node->item = item;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return (new_node);
+}
