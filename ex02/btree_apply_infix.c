541d0902966a9be7c65db847d4e72a0d57c4a2ec
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 07:32:22 +0800

    done with ex02

--- ex02/btree_apply_infix.c
+++ ex02/btree_apply_infix.c
@@ -6,7 +6,21 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 07:23:23 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 07:23:24 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 07:32:18 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_btree.h"
+#include <stdlib.h>
+
+void btree_apply_infix(t_btree *root, void (*applyf)(void *))
+{
+    if (root == NULL)
+        return;
+
+    btree_apply_infix(root->left, applyf);
+
+    applyf(root->item);
+
+    btree_apply_infix(root->right, applyf);
+}
--- ex02/ft_btree.h
+++ ex02/ft_btree.h
@@ -6,7 +6,20 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 07:21:51 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 07:21:52 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 07:31:52 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_BTREE_H
+#define FT_BTREE_H
+
+typedef struct s_btree
+{
+    struct s_btree *left;
+    struct s_btree *right;
+    void            *item;
+} t_btree;
+
+void btree_apply_infix(t_btree *root, void (*applyf)(void *));
+
+#endif
