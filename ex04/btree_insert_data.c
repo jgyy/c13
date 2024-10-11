e4ad6a8f5eba56c2026f4054eab22ca6adb9a06d
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 07:53:25 +0800

    done with ex04

--- ex04/btree_insert_data.c
+++ ex04/btree_insert_data.c
@@ -6,7 +6,38 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 07:23:27 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 07:23:28 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 07:53:15 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_btree.h"
+#include <stdlib.h>
+
+t_btree *btree_create_node(void *item)
+{
+    t_btree *new_node;
+
+    new_node = (t_btree *)malloc(sizeof(t_btree));
+    if (new_node == NULL)
+        return (NULL);
+    new_node->item = item;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return (new_node);
+}
+
+void btree_insert_data(t_btree **root, void *item, int (*cmpf)(void *, void *))
+{
+    if (*root == NULL)
+    {
+        *root = btree_create_node(item);
+        return;
+    }
+
+    int comparison = cmpf(item, (*root)->item);
+
+    if (comparison < 0)
+        btree_insert_data(&((*root)->left), item, cmpf);
+    else
+        btree_insert_data(&((*root)->right), item, cmpf);
+}
